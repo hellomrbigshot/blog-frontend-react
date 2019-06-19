4047e785d2cd46ac97012e2e4e329decb50353c0
hellomrbigshot <1994yechen@gmail.com>
2019-06-19 17:55:43 +0800

    update taglist page

--- src/views/tag/index.js
+++ src/views/tag/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
-import { TagWrapper } from './styled'
+import { TagWrapper, Header, ListWrapper, TagItem, TagHeader, TagDesc, TagBottom } from './styled'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Pagination } from 'antd'
 import { actionCreators } from './store'
+
 
 class TagList extends Component {
   render() {
+    const { total, tagList, pageChange } = this.props
     return (
       <TagWrapper>
-        
+        <Header>当前总共 {total} 个标签</Header>
+        <ListWrapper>
+          {
+            tagList.size === 0 ? null : 
+            tagList.map((tag, i) => (
+              <TagItem key={tag.get('_id')}>
+                <TagHeader>
+                  <Link to="">{tag.get('name')}</Link>
+                </TagHeader>
+                <TagDesc>{tag.get('description')}</TagDesc>
+                <TagBottom>共有 {tag.get('page_num')} 篇文章</TagBottom>
+              </TagItem>
+            ))
+          }
+        </ListWrapper>
+        <Pagination total={total} onChange={pageChange} />
       </TagWrapper>
     )
   }
@@ -25,6 +44,10 @@ const mapDispatchToProps = (dispatch) => {
   return {
     getTagList() {
       dispatch(actionCreators.getTagList())
+    },
+    pageChange(page) {
+      dispatch(actionCreators.getTagList(page))
+      window.scroll(0, 0)
     }
   }
 }
--- src/views/tag/styled.js
+++ src/views/tag/styled.js
@@ -4,3 +4,49 @@ export const TagWrapper = styled.div`
   width: 800px;
   margin: 100px auto;
 `
+
+export const Header = styled.h2`
+  font-size: 14px;
+  font-weight: normal;
+`
+
+export const ListWrapper = styled.div`
+  margin: 26px 0;
+`
+
+export const TagItem = styled.div`
+  margin-bottom: 26px;
+`
+
+export const TagHeader = styled.div`
+  overflow: hidden;
+  a {
+    display: block;
+    margin-bottom: 5px;
+    font-size: 18px;
+    font-weight: 400;
+    color: #333;
+    line-height: 20px;
+    border-bottom: 1.5px solid #fff;
+    float: left;
+    &:hover {
+      color: #333;
+      border-bottom-color: #333;
+    }
+  }
+`
+
+export const TagDesc = styled.div`
+  font-size: 13px;
+  word-wrap: break-word;
+  line-height: 1.4;
+  margin-bottom: 3px;
+  font-weight: normal;
+  color: #555;
+`
+
+export const TagBottom = styled.div`
+  line-height: 1.4;
+  color: #222;
+  font-size: 12px;
+`
