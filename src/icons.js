3eb34f0cd733b31e83e761430b729882d4a0cf09
hellomrbigshot <1994yechen@gmail.com>
2019-07-22 10:38:00 +0800

    format code

--- src/icons.js
+++ src/icons.js
@@ -1,21 +1,7 @@
-export {
-  default as UserOutline
-} from '@ant-design/icons/lib/outline/UserOutline'
-export {
-  default as LockOutline
-} from '@ant-design/icons/lib/outline/LockOutline'
-export {
-  default as EyeInvisibleOutline
-} from '@ant-design/icons/lib/outline/EyeInvisibleOutline'
-export {
-  default as Eye
-} from '@ant-design/icons/lib/outline/EyeOutline'
-export {
-  default as Check
-} from '@ant-design/icons/lib/outline/CheckOutline'
-export {
-  default as CloseCircleOutline
-} from '@ant-design/icons/lib/outline/CloseCircleOutline'
-export {
-  default as CloseOutline
-} from '@ant-design/icons/lib/outline/CloseOutline'
+export { default as UserOutline } from '@ant-design/icons/lib/outline/UserOutline'
+export { default as LockOutline } from '@ant-design/icons/lib/outline/LockOutline'
+export { default as EyeInvisibleOutline } from '@ant-design/icons/lib/outline/EyeInvisibleOutline'
+export { default as Eye } from '@ant-design/icons/lib/outline/EyeOutline'
+export { default as Check } from '@ant-design/icons/lib/outline/CheckOutline'
+export { default as CloseCircleOutline } from '@ant-design/icons/lib/outline/CloseCircleOutline'
+export { default as CloseOutline } from '@ant-design/icons/lib/outline/CloseOutline'
