7336bbdaf86a4d319b0d2fe04705e129716ae9bb
hellomrbigshot <1994yechen@gmail.com>
2019-07-24 23:18:29 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -6,31 +6,27 @@ const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
 
 const Custom = config => {
     let plugins = [
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-            ignoreOrder: true
-        }),
-        new ParallelUglifyPlugin({
-            cacheDir: '.cache/',
-            uglifyJS: {
-                output: {
-                    comments: false
-                },
-                warnings: false
-            },
-            sourceMap: false
-        }),
-        new CompressionWebpackPlugin({
-            filename: '[path].gz[query]',
-            algorithm: 'gzip',
-            test: new RegExp('\\.(js|css)$'),
-            threshold: 10240,
-            minRatio: 0.8
-        })
+        // new MiniCssExtractPlugin({
+        //     filename: '[name].css',
+        //     chunkFilename: '[id].css',
+        //     ignoreOrder: true
+        // }),
+        // new ParallelUglifyPlugin({
+        //     uglifyJS: {
+        //         output: {
+        //             comments: false
+        //         },
+        //         warnings: false
+        //     }
+        // }),
+        // new CompressionWebpackPlugin({
+        //     filename: '[path].gz[query]',
+        //     algorithm: 'gzip',
+        //     test: new RegExp('\\.(js|css)$'),
+        //     threshold: 10240,
+        //     minRatio: 0.8
+        // })
     ]
-    console.log(process.env)
-    debugger
     if (process.env.NODE_ENV !== 'development') {
         config.plugins = [...config.plugins, ...plugins]
     }
@@ -43,17 +39,7 @@ module.exports = override(
         style: 'css'
     }),
     addWebpackExternals({
-        hljs: 'hljs',
-        marked: 'marked',
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        'react-router-dom': 'ReactRouterDOM',
-        redux: 'Redux',
-        'react-redux': 'ReactRedux',
-        'redux-saga': 'ReduxSaga',
-        'redux-saga/effects': 'ReduxSagaEffects',
-        immutable: 'Immutable',
-        axios: 'axios'
+        hljs: 'hljs'
     }),
     addWebpackAlias({
         '@ant-design/icons/lib/dist$': path.resolve(__dirname, './src/icons.js')
