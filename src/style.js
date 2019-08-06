ddd4653575fdeebebf12a26efe5369956b76e22d
hellomrbigshot <1994yechen@gmail.com>
2019-08-06 10:07:14 +0800

    add vue-icon

--- src/style.js
+++ src/style.js
@@ -60,4 +60,10 @@ menu, nav, output, ruby, section, summary {
 #root {
   padding-top: 1px;
 }
+.icon {
+  width: 1em; height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 `
