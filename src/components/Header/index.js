6b870e8b5ece290c88672355538e48aa6ca3121a
hellomrbigshot <1994yechen@gmail.com>
2020-01-21 18:43:42 +0800

    ✨ react hooks 初始化

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,194 +1,181 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import { CSSTransition } from 'react-transition-group'
 import classnames from 'classnames'
 import {
-    HeaderWrapper,
-    Logo,
-    SearchWrapper,
-    Nav,
-    NavSearch,
-    NavItem,
-    Addition,
-    Button,
-    AvatarWrapper,
-    AvatarContent,
-    DropdownWrapper,
-    DropdownItem
+  HeaderWrapper,
+  Logo,
+  SearchWrapper,
+  Nav,
+  NavSearch,
+  NavItem,
+  Addition,
+  Button,
+  AvatarWrapper,
+  AvatarContent,
+  DropdownWrapper,
+  DropdownItem
 } from './style'
 import { actionCreators } from './store'
 import { actionCreators as loginCreator } from '../../views/user/store'
 
-class Header extends Component {
-    render() {
-        const {
-            inputFocus,
-            inputBlur,
-            focused,
-            location: { pathname },
-            user,
-            mouseIn,
-            handleMouseEnter,
-            handleMouseLeave
-        } = this.props
-        const hideHeaderPath = ['/login', '/register', '/404']
-        const hideHeader = hideHeaderPath.includes(pathname.trim())
-        const vueIcon = `<use xlink:href='#icon-vue' />`
-        return hideHeader ? null : (
-            <HeaderWrapper>
-                <Link to="/">
-                    <Logo />
-                </Link>
-                <Nav>
-                    <SearchWrapper>
-                        <CSSTransition in={focused} timeout={400} classNames="slide">
-                            <NavSearch
-                                placeholder="搜索"
-                                onFocus={inputFocus}
-                                onBlur={inputBlur}
-                                className={focused ? 'focused' : ''}
-                                ref={input => {
-                                    this.keywords = input
-                                }}
-                                onKeyDown={event => this.handleKeyDown(event, this.keywords)}
-                            />
-                        </CSSTransition>
-                        <i
-                            onClick={() => this.handleSearch(this.keywords)}
-                            className={classnames('iconfont', 'icon-fangdajing', { focused: focused })}
-                        />
-                    </SearchWrapper>
-                    <NavItem className="first-nav">
-                        <Link to="/tag/list">标签</Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to="/comment/list">留言</Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link to="/lab/list">实验室</Link>
-                    </NavItem>
-                    <NavItem>
-                        <a href="https://www.hellomrbigbigshot.xyz">
-                        <svg
-                          className="icon"
-                          style={{ fontSize: '16px' }}
-                          aria-hidden="true" dangerouslySetInnerHTML={{ __html: vueIcon }}
-                        />
-                          版本
-                        </a>
-                    </NavItem>
-                </Nav>
-                <Addition>
-                    <Link to="/write">
-                        <Button className="write">
-                            <i className={classnames('iconfont', 'icon-yumaobi')} />
-                            写文章
-                        </Button>
-                    </Link>
-                    {user ? (
-                        <AvatarWrapper onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                            <AvatarContent src={`/api/file/avatar/user/?username=${user}`} />
-                            <CSSTransition in={mouseIn} timeout={400} classNames="fade">
-                                {this.getDropDown()}
-                            </CSSTransition>
-                        </AvatarWrapper>
-                    ) : (
-                        <Fragment>
-                            <Link to="/register">
-                                <Button className="reg">注册</Button>
-                            </Link>
-                            <Link to="/login">
-                                <Button className="login">登录</Button>
-                            </Link>
-                        </Fragment>
-                    )}
-                </Addition>
-            </HeaderWrapper>
-        )
+function Header(props) {
+  let keywords = ''
+  const getDropDown = () => {
+    const { logout, user, handleMouseEnter, handleMouseLeave, mouseIn } = props
+    return (
+      <DropdownWrapper className={mouseIn ? 'mouse-in' : ''} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <DropdownItem style={{ marginTop: '5px' }}>
+          <Link to={`/user/info/${user}`}>
+            <i className="iconfont icon-UserSettings" />
+            <span>我的主页</span>
+          </Link>
+        </DropdownItem>
+        <DropdownItem>
+          <Link to="/user/list">
+            <i className="iconfont icon-blogs" />
+            <span>我的文章</span>
+          </Link>
+        </DropdownItem>
+        <DropdownItem>
+          <Link to="/user/draft">
+            <i className="iconfont icon-draft" />
+            <span>我的草稿</span>
+          </Link>
+        </DropdownItem>
+        <DropdownItem>
+          <Link to="/comment/list">
+            <i className="iconfont icon-comment" />
+            <span>我的评论</span>
+          </Link>
+        </DropdownItem>
+        <DropdownItem>
+          <div onClick={logout}>
+            <i className="iconfont icon-signout_detail_toil" />
+            <span>退出</span>
+          </div>
+        </DropdownItem>
+      </DropdownWrapper>
+    )
+  }
+  const handleKeyDown = (event, keywords) => {
+    const KEYCODE = event.keyCode
+    const KEYWORDS = keywords.value
+    if (KEYCODE === 13 && KEYWORDS) {
+      // enter 触发搜索
+      props.history.push(`/home/${KEYWORDS}`)
     }
-    getDropDown() {
-        const { logout, user, handleMouseEnter, handleMouseLeave, mouseIn } = this.props
-        return (
-            <DropdownWrapper onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={mouseIn ? 'mouse-in' : ''}>
-                <DropdownItem style={{ marginTop: '5px' }}>
-                    <Link to={`/user/info/${user}`}>
-                        <i className="iconfont icon-UserSettings" />
-                        <span>我的主页</span>
-                    </Link>
-                </DropdownItem>
-                <DropdownItem>
-                    <Link to="/user/list">
-                        <i className="iconfont icon-blogs" />
-                        <span>我的文章</span>
-                    </Link>
-                </DropdownItem>
-                <DropdownItem>
-                    <Link to="/user/draft">
-                        <i className="iconfont icon-draft" />
-                        <span>我的草稿</span>
-                    </Link>
-                </DropdownItem>
-                <DropdownItem>
-                    <Link to="/comment/list">
-                        <i className="iconfont icon-comment" />
-                        <span>我的评论</span>
-                    </Link>
-                </DropdownItem>
-                <DropdownItem>
-                    <div onClick={logout}>
-                        <i className="iconfont icon-signout_detail_toil" />
-                        <span>退出</span>
-                    </div>
-                </DropdownItem>
-            </DropdownWrapper>
-        )
+  }
+  const handleSearch = keywords => {
+    if (keywords) {
+      props.history.push(`/home/${keywords.value}`)
     }
-    handleKeyDown(event, keywords) {
-        const KEYCODE = event.keyCode
-        const KEYWORDS = keywords.value
-        if (KEYCODE === 13 && KEYWORDS) {
-            // enter 触发搜索
-            this.props.history.push(`/home/${KEYWORDS}`)
-        }
-    }
-    handleSearch(keywords) {
-        if (keywords) {
-            this.props.history.push(`/home/${keywords.value}`)
-        }
-    }
+  }
+  const {
+    inputFocus,
+    inputBlur,
+    focused,
+    location: { pathname },
+    user,
+    mouseIn,
+    handleMouseEnter,
+    handleMouseLeave
+  } = props
+  const hideHeaderPath = ['/login', '/register', '/404']
+  const hideHeader = hideHeaderPath.includes(pathname.trim())
+  const vueIcon = `<use xlink:href='#icon-vue' />`
+  return hideHeader ? null : (
+    <HeaderWrapper>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <Nav>
+        <SearchWrapper>
+          <CSSTransition in={focused} timeout={400} classNames="slide">
+            <NavSearch
+              placeholder="搜索"
+              onFocus={inputFocus}
+              onBlur={inputBlur}
+              className={focused ? 'focused' : ''}
+              ref={input => {
+                keywords = input
+              }}
+              onKeyDown={event => handleKeyDown(event, keywords)}
+            />
+          </CSSTransition>
+          <i onClick={() => handleSearch(keywords)} className={classnames('iconfont', 'icon-fangdajing', { focused: focused })} />
+        </SearchWrapper>
+        <NavItem className="first-nav">
+          <Link to="/tag/list">标签</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/comment/list">留言</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/lab/list">实验室</Link>
+        </NavItem>
+        <NavItem>
+          <a href="https://www.hellomrbigbigshot.xyz">
+            <svg className="icon" style={{ fontSize: '16px' }} aria-hidden="true" dangerouslySetInnerHTML={{ __html: vueIcon }} />
+            版本
+          </a>
+        </NavItem>
+      </Nav>
+      <Addition>
+        <Link to="/write">
+          <Button className="write">
+            <i className={classnames('iconfont', 'icon-yumaobi')} />
+            写文章
+          </Button>
+        </Link>
+        {user ? (
+          <AvatarWrapper onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <AvatarContent src={`/api/file/avatar/user/?username=${user}`} />
+            <CSSTransition in={mouseIn} timeout={400} classNames="fade">
+              {getDropDown()}
+            </CSSTransition>
+          </AvatarWrapper>
+        ) : (
+          <Fragment>
+            <Link to="/register">
+              <Button className="reg">注册</Button>
+            </Link>
+            <Link to="/login">
+              <Button className="login">登录</Button>
+            </Link>
+          </Fragment>
+        )}
+      </Addition>
+    </HeaderWrapper>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-        focused: state.getIn(['header', 'focused']),
-        user: state.getIn(['user', 'user']),
-        mouseIn: state.getIn(['header', 'mouseIn'])
-    }
+  return {
+    focused: state.getIn(['header', 'focused']),
+    user: state.getIn(['user', 'user']),
+    mouseIn: state.getIn(['header', 'mouseIn'])
+  }
 }
 const mapDispatchToProps = dispatch => {
-    return {
-        inputFocus() {
-            dispatch(actionCreators.searchFocus())
-        },
-        inputBlur() {
-            dispatch(actionCreators.searchBlur())
-        },
-        logout() {
-            dispatch(loginCreator.logout())
-        },
-        handleMouseEnter() {
-            dispatch(actionCreators.mouseIn())
-        },
-        handleMouseLeave() {
-            dispatch(actionCreators.mouseLeave())
-        }
+  return {
+    inputFocus() {
+      dispatch(actionCreators.searchFocus())
+    },
+    inputBlur() {
+      dispatch(actionCreators.searchBlur())
+    },
+    logout() {
+      dispatch(loginCreator.logout())
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseIn())
+    },
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave())
     }
+  }
 }
 // export default connect(mapStateToProps, mapDispatchToProps)(Header)
-export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(Header)
-)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header))
--- src/views/user/index.js
+++ src/views/user/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Pagination, Avatar, Icon, Message } from 'antd'
 import { Link } from 'react-router-dom'
@@ -6,164 +6,133 @@ import { getUserInfo, getLimitArticleList } from './store/actionCreators'
 import { post } from '../../common/fetch'
 import AvatarCropper from './components/AvatarCropper'
 import BioEditModal from './components/BioEditModal'
-import { 
-          UserInfoWrapper, 
-          UserAvatarWrapper, 
-          UserInfoDetailWrapper, 
-          BioWrapper, 
-          LimitArticleList, 
-          LimitArticleItem,
-          AvatarSelectButton
-        } from './styled'
+import {
+  UserInfoWrapper,
+  UserAvatarWrapper,
+  UserInfoDetailWrapper,
+  BioWrapper,
+  LimitArticleList,
+  LimitArticleItem,
+  AvatarSelectButton
+} from './styled'
 
-class UserInfo extends Component {
-    constructor (props) {
-      super(props)
-      this.state = {
-        avatarModalVisible: false,
-        bioModalVisible: false,
-        img: '',
-        cropImgData: ''
-      }
+function UserInfo (props) {
+  const [avatarModalVisible, setAvatarModalVisible] = useState(false)
+  const [bioModalVisible, setBioModalVisible] = useState(false)
+  const [img, setImg] = useState('')
+  const [cropImgData, setCropImgData] = useState('')
+  const { name: user } = props.match.params
+  useEffect(() => {
+    props.getUserInfo(user)
+    props.getLimitArticleList()
+  }, [props, props.match.params.name, user])
+  const handleFileChange = file => {
+    if (file) {
+      const URL = window.URL || window.webkitURL
+      setAvatarModalVisible(true)
+      setImg(URL.createObjectURL(file))
+    } else {
+      return false
     }
-    handleFileChange = (file) => {
-      if (file) {
-        const URL = window.URL || window.webkitURL
-        this.setState(() => ({
-          avatarModalVisible: true,
-          img: URL.createObjectURL(file)
-        }))
-      } else {
-        return false
-      } 
+  }
+  const handleCropperChange = imgData => {
+    if (imgData) {
+      setCropImgData(imgData)
     }
-    handleCropperChange = (imgData) => {
-      if (imgData) {
-        this.setState(() => ({
-          cropImgData: imgData
-        }))
-      }
+  }
+  const handleCropperCancel = () => {
+    setImg('')
+    setCropImgData('')
+    setAvatarModalVisible(false)
+  }
+  const handleCropperOk = async imgData => {
+    const { loginUser } = props
+    await post('/api/file/uploadAvatar', { imgData, username: loginUser })
+    window.location.reload()
+  }
+  const handleBioOk = async bio => {
+    if (!bio.trim()) {
+      Message.error('描述不能为空')
+      return false
     }
-    handleCropperCancel = (imgData) => {
-      this.setState(() => ({
-        img: '',
-        cropImgData: '',
-        avatarModalVisible: false
-      }))
-    }
-    handleCropperOk = async (imgData) => {
-      const { loginUser } = this.props
-      await post('/api/file/uploadAvatar', { imgData, username: loginUser })
-      window.location.reload()
-    }
-    handleBioOk = async (bio) => {
-      if (!bio.trim()) {
-        Message.error('描述不能为空')
-        return false
-      }
-      const username = this.props.loginUser
-      await post('/api/user/updatebio', { username, bio })
-      this.setState(() => ({
-        bioModalVisible: false
-      }))
-      this.props.getUserInfo()
-    }
-    render() {
-        const { total, userInfo, articleList, loginUser, getLimitArticleList } = this.props
-        const { avatarModalVisible, bioModalVisible, img, cropImgData } = this.state
-        const { name: user } = this.props.match.params
-        const canEdit = loginUser === user
-        return (
-            <div>
-                <UserInfoWrapper>
-                    <UserAvatarWrapper>
-                        <Avatar size={150} src={`/api/file/avatar/user/?username=${user}`} />
-                        {
-                          canEdit ? (
-                            <AvatarSelectButton className="avatar-select">更换头像
-                              <input type="file" onChange={(e) => this.handleFileChange(e.target.files[0])} />
-                            </AvatarSelectButton>
-                          ) : null
-                        }
-                    </UserAvatarWrapper>
-                    <UserInfoDetailWrapper>
-                        <h2>{user}</h2>
-                        <BioWrapper>
-                          {userInfo.get('bio') || '暂时没有个人简介(lll￢ω￢)'}
-                          {
-                            canEdit ? 
-                              <Icon
-                                className='editIcon'
-                                type='edit'
-                                onClick={() => this.setState(() => ({ bioModalVisible: true }))}
-                              /> : null
-                          }
-                          
-                        </BioWrapper>
-                    </UserInfoDetailWrapper>
-                </UserInfoWrapper>
-                <LimitArticleList>
-                    <h2>相关文章</h2>
-                    {articleList.map(article => (
-                        <LimitArticleItem key={article.get('_id')}>
-                            <Link to={`/detail/${article.get('_id')}`}>
-                                <h1 className="title">{article.get('title')}</h1>
-                            </Link>
-                            <div className="time">{article.get('create_time').slice(5, 10)}</div>
-                        </LimitArticleItem>
-                    ))}
-                </LimitArticleList>
-                {total > 10 ? <Pagination total={total} onChange={getLimitArticleList} /> : null}
-                <AvatarCropper
-                    visible={avatarModalVisible}
-                    img={img}
-                    cropImgData={cropImgData}
-                    onCropper={imgData => this.handleCropperChange(imgData)}
-                    onCancel={imgData => this.handleCropperCancel(imgData)}
-                    onOk={imgData => this.handleCropperOk(imgData)}
-                />
-                {
-                  userInfo.get('username') ?
-                    <BioEditModal
-                      visible={bioModalVisible}
-                      onCancel={() => this.setState(() => ({ bioModalVisible: false }))}
-                      onOk={(input) => this.handleBioOk(input)}
-                    />
-                    : null
-                }
-            </div>
-        )
-    }
-    componentDidMount() {
-        const user = this.props.match.params.name
-        this.props.getUserInfo(user)
-        this.props.getLimitArticleList()
-    }
+    const username = props.loginUser
+    await post('/api/user/updatebio', { username, bio })
+    setBioModalVisible(false)
+    props.getUserInfo()
+  }
+  const { total, userInfo, articleList, loginUser, getLimitArticleList } = props
+  const canEdit = loginUser === user
+  return (
+    <div>
+      <UserInfoWrapper>
+        <UserAvatarWrapper>
+          <Avatar size={150} src={`/api/file/avatar/user/?username=${user}`} />
+          {canEdit ? (
+            <AvatarSelectButton className="avatar-select">
+              更换头像
+              <input type="file" onChange={e => handleFileChange(e.target.files[0])} />
+            </AvatarSelectButton>
+          ) : null}
+        </UserAvatarWrapper>
+        <UserInfoDetailWrapper>
+          <h2>{user}</h2>
+          <BioWrapper>
+            {userInfo.get('bio') || '暂时没有个人简介(lll￢ω￢)'}
+            {canEdit ? <Icon className="editIcon" type="edit" onClick={() => setBioModalVisible(true)} /> : null}
+          </BioWrapper>
+        </UserInfoDetailWrapper>
+      </UserInfoWrapper>
+      <LimitArticleList>
+        <h2>相关文章</h2>
+        {articleList.map(article => (
+          <LimitArticleItem key={article.get('_id')}>
+            <Link to={`/detail/${article.get('_id')}`}>
+              <h1 className="title">{article.get('title')}</h1>
+            </Link>
+            <div className="time">{article.get('create_time').slice(5, 10)}</div>
+          </LimitArticleItem>
+        ))}
+      </LimitArticleList>
+      {total > 10 ? <Pagination total={total} onChange={getLimitArticleList} /> : null}
+      <AvatarCropper
+        visible={avatarModalVisible}
+        img={img}
+        cropImgData={cropImgData}
+        onCropper={imgData => handleCropperChange(imgData)}
+        onCancel={imgData => handleCropperCancel(imgData)}
+        onOk={imgData => handleCropperOk(imgData)}
+      />
+      {userInfo.get('username') ? (
+        <BioEditModal
+          visible={bioModalVisible}
+          onCancel={() => setBioModalVisible(false)}
+          onOk={input => handleBioOk(input)}
+        />
+      ) : null}
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-        articleList: state.getIn(['user', 'userInfo', 'articleList']),
-        userInfo: state.getIn(['user', 'userInfo', 'info']),
-        total: state.getIn(['user', 'userInfo', 'total']),
-        loginUser: state.getIn(['user', 'user'])
-    }
+  return {
+    articleList: state.getIn(['user', 'userInfo', 'articleList']),
+    userInfo: state.getIn(['user', 'userInfo', 'info']),
+    total: state.getIn(['user', 'userInfo', 'total']),
+    loginUser: state.getIn(['user', 'user'])
+  }
 }
 
 const mapDispatchToProps = (dispatch, props) => {
-    return {
-        getUserInfo() {
-            const name = props.match.params.name
-            dispatch(getUserInfo(name))
-        },
-        getLimitArticleList(page = 1) {
-            const { name } = props.match.params
-            dispatch(getLimitArticleList(name, page))
-        }
+  return {
+    getUserInfo() {
+      const name = props.match.params.name
+      dispatch(getUserInfo(name))
+    },
+    getLimitArticleList(page = 1) {
+      const { name } = props.match.params
+      dispatch(getLimitArticleList(name, page))
     }
+  }
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(UserInfo)
+export default connect(mapStateToProps, mapDispatchToProps)(UserInfo)
